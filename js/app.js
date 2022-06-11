1d4b261e73f7f4488881b7394f8e2871b7509e8f
creatiko <39159187+creatiko@users.noreply.github.com>
2022-06-11 07:57:57 -0400

    Fixed: Added and removed items as per code review instructions

--- js/app.js
+++ js/app.js
@@ -29,7 +29,15 @@
  * Start Helper Functions
  * 
  */
+
 window.addEventListener('DOMContentLoaded', () => {
+	
+	/**
+	 * 
+	 * Togles the navbar on mobile viewport
+	 * 
+	 */
+	
 	const menuToggler = document.getElementById('navbar-toggler');
 	const mobileMenu = document.getElementById('navbarResponsive');
 	menuToggler.classList.add('collapsed');
@@ -42,14 +50,12 @@ window.addEventListener('DOMContentLoaded', () => {
 			menuToggler.classList.add('collapsed');
 		}
 	});
-	document.querySelectorAll('.nav-item').forEach(item => {
-		item.addEventListener('click', event => {
-			//handle click on toggled navigation
-			mobileMenu.classList.remove('show');
-			menuToggler.classList.add('collapsed');
-		})
-	});
-	// lets create section 4 programatically
+
+	/**
+	 * 
+	 * creates section 4 programatically
+	 * 
+	 */
 	const mainContent = document.querySelector('main');
 	const newSection = document.createElement('section');
 	newSection.setAttribute('id', 'section4');
@@ -59,8 +65,20 @@ window.addEventListener('DOMContentLoaded', () => {
 	const newSectionp1 = document.createElement('p');
 	const newSectionp2 = document.createElement('p');
 	newSectionh2.innerHTML = 'Section 4';
-	newSectionp1.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
-	newSectionp2.innerHTML = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
+	newSectionp1.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
+	newSectionp1.innerHTML += 'Morbi fermentum metus faucibus lectus pharetra dapibus. ';
+	newSectionp1.innerHTML += 'Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. ';
+	newSectionp1.innerHTML += 'Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. ';
+	newSectionp1.innerHTML += 'Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, ';
+	newSectionp1.innerHTML += 'nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. ';
+	newSectionp1.innerHTML += 'Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, ';
+	newSectionp1.innerHTML += 'aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, ';
+	newSectionp1.innerHTML += 'faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. ';
+	newSectionp1.innerHTML += 'Sed congue et odio sed euismod.';
+	newSectionp2.innerHTML = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ';
+	newSectionp2.innerHTML += 'ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. ';
+	newSectionp2.innerHTML += 'Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, ';
+	newSectionp2.innerHTML += 'eget elementum tortor mollis non.';
 	newSectionDiv.setAttribute('class', 'landing__container');
 	newSection.appendChild(newSectionDiv);
 	newSectionDiv.appendChild(newSectionh2);
@@ -74,14 +92,20 @@ window.addEventListener('DOMContentLoaded', () => {
 	 * 
 	 */
 
-	// build the nav
-	//find the nav container
+	/**
+	 * 
+	 * builds the nav 
+	 * first find the nav container by id
+	 * finds all the sections with attribue data-nav
+	 * and creates an li element for each
+	 * lastly appends the li elements to the ul element
+	 * 
+	 */
+
 	const navContainer = document.getElementById('navbar__list');
-	//find all the sections to the name of the data-nav
 	document.querySelectorAll('section').forEach(item => {
 		let dataNav = item.getAttribute('data-nav');
 		let dataId = item.getAttribute('id');
-		//create an li element for each
 		let newLI = document.createElement('li');
 		newLI.setAttribute('class', 'nav-item');
 		let newA = document.createElement('a');
@@ -90,26 +114,27 @@ window.addEventListener('DOMContentLoaded', () => {
 		newA.setAttribute('id', `${dataId}Nav`);
 		newA.innerHTML = dataNav;
 		newLI.appendChild(newA);
-		//append it to the container
 		navContainer.appendChild(newLI);
 	})
 
-	// Add class 'active' to section when near top of viewport
-	// on window scroll
+	/**
+	 * Adds class 'active' to section when near top of viewport on scroll
+	 * first it finds the sections <section> on the page and loops to
+	 * get the distance from the top
+	 * if the distance to the top is between 0-100px adds active-section
+	 * 
+	 */
+
 	window.addEventListener('scroll', function () {
-		// get the sections on the page and loop
+		// 
 		const sections = document.querySelectorAll('section');
 		sections.forEach(section => {
 			let sectionID = section.getAttribute('id');
 			let navID = document.getElementById(`${sectionID}Nav`)
-			// get the distance from top
 			const currentTop = section.getBoundingClientRect().top;
-			//console.log(currentTop);
-			// if the distance to the top is between 0-100px
 			if (currentTop >= -5 && currentTop < 200) {
 				section.classList.add('active-section');
 				navID.classList.add('active-nav');
-				// otherwise, remove the class
 			} else {
 				section.classList.remove('active-section');
 				navID.classList.remove('active-nav');
@@ -117,22 +142,32 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	// Scroll to anchor ID using scrollTO event
+	/**
+	 * 
+	 * Scroll to anchor ID using scrollTO event
+	 * 
+	 */
+	
 	document.querySelectorAll('a').forEach(link => {
 		link.addEventListener('click', event => {
 			event.preventDefault();
+			mobileMenu.classList.remove('show');
+			menuToggler.classList.add('collapsed');
 			const sectionId = link.getAttribute('href');
-			//console.log(sectionId);
 			const section = document.querySelector(sectionId);
-			section.scrollIntoView();
+			section.scrollIntoView({behavior: 'smooth'});
 		});
 	});
-	
-	// Scroll to Top
+
+	/**
+	 * 
+	 * Scroll to Top
+	 * 
+	 */
+
 	window.addEventListener('scroll', function () {
-	const scrollBtn = document.getElementById('scrollToTop');
+		const scrollBtn = document.getElementById('scrollToTop');
 		const currentTop = document.body.getBoundingClientRect().top * -1;
-		//console.log(currentTop);
 		if (currentTop > 40) {
 			scrollBtn.style.display = 'inline';
 		} else {
